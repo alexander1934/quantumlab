599ad17ccd59eb63c03c052d7a2bae208012f2a5
alexander1934 <alexankovalnko23@gmail.com>
2024-10-14 00:59:56 +0300

    create next app structure update

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import Input from "../../../../components/Input";
+import Link from "next/link";
+import Button from "../../../../components/Button";
+import styles from "@/../styles/pages/Home.module.scss";
+
+export default function Home() {
+    return (
+        <div>
+            <main className={styles.main}>
+                <div className={styles.form}>
+                    <div className={styles.form__container}>
+                        <h2 className={styles.form__title}>Начать</h2>
+                        <div>
+                            <Input placeholder={"Как вас зовут?"} title={"Ваше имя"}/>
+                        </div>
+                    </div>
+                    <nav className={styles.button__container}>
+                        <Link className={styles.link__container} href="/calculator" passHref>
+                            <Button title={"Открыть калькулятор"}/>
+                        </Link>
+                        <Link className={styles.link__container} href="/generator" passHref>
+                            <Button title={"Открыть генератор"}/>
+                        </Link>
+                    </nav>
+                </div>
+            </main>
+        </div>
+    );
+}
